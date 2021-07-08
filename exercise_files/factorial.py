5bcdc2caa25b65c86ee51fbd5ef1d2d9f787599d
Patibandha <meet5627@gmail.com>
2021-07-08 16:56:37 -0700

    class 7/8/2021

--- exercise_files/factorial.py
+++ exercise_files/factorial.py
@@ -8,11 +8,19 @@ n! = n*(n-1)*(n-2).....*1
 (3) nosetest
 (4) request factory
 (5) mock.Mock()
-"""
 
+logging is the module
+there main logging
+(1) logging.info --- this is informative log for your code or development
+(2) logging.warning --- this is warning log (message) generated from your code
+(3) logging.error --- this is error log (message) generated from your code, it break the code
+
+"""
+import logging
 
 def fact(*args):
     try:
+        logging.info(msg='I am inside try block of fact method')
         val = args[0]
         fact_result = 1
         if val >= 0:
@@ -23,6 +31,7 @@ def fact(*args):
             return "Please provide positive number!"
 
     except TypeError as err:
+        logging.error(msg="the code break")
         return err
 
 
