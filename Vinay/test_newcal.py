5d5ac3ba98db15a636b0c007d7af33a47a887fbe
vinaykathiria <vinaykathiria@gmail.com>
2021-07-09 16:15:58 -0400

    No errors were found, but final answer can't be shown. please leave me a comment thanks.

--- Vinay/test_newcal.py
+++ Vinay/test_newcal.py
@@ -1,6 +1,21 @@
-from newcal import add
+from newcal import add, subtract, multiply, divide
 
 
 def test_add():
-    res = add("%%%%%", 40)
+    res = add(20, 40)
     assert res == 60
+
+
+def test_subtract():
+    res = subtract(20, 10)
+    assert res == 10
+
+
+def test_multiply():
+    res = multiply(5, 10)
+    assert res == 50
+
+
+def test_divide():
+    res = divide(10, 2)
+    assert res == 5
