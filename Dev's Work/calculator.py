78453f42dc547bea03f7d61caa134ae723193a2c
Dev S <33499530+shahdev10@users.noreply.github.com>
2021-07-22 13:51:34 -0700

    Update calculator.py

--- Dev's Work/calculator.py
+++ Dev's Work/calculator.py
@@ -8,19 +8,21 @@ class Operation(val1, val2):
     def divide(self, val1, val2):
         return val1 // val2
 
+calculate = Operation(val1, val2)
+
 def main_method():
     print("Enter an operation from +, - , /, *")
     operator = input("enter here:")
     val1 = input("Enter your 1st number:")
     val2 = input("Enter your 2nd number:")
     if operator == '+':
-        print(Operation.add(val1, val2))
+        print(calculate.add(val1, val2))
     elif operator == '-':
-        print(Operation.subtract(val1, val2))
+        print(calculate.subtract(val1, val2))
     elif operator == '/':
-        print(Operation.divide(val2, val1))
+        print(calculate.divide(val2, val1))
     elif operator == '*':
-        print(Operation.multiply(val2, val1))
+        print(calculate.multiply(val2, val1))
     else:
         print("Choose the correct option")
 if __name__ == '__main__':
