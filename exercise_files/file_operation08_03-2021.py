0f7fef2ff8b71ff8426c94442b91c199ef361be8
Patibandha <meet5627@gmail.com>
2021-08-03 17:00:32 -0700

    class 8/03/2021

---
+++ exercise_files/file_operation08_03-2021.py
@@ -0,0 +1,18 @@
+"""
+This is file operation exercises
+"""
+import os
+
+
+def read_write_op(ab_path, file_name):
+    fil = os.path.join(ab_path, file_name)
+    with open(fil, 'w+') as f:
+        f.write("This is Awesome file\n")
+
+
+if __name__ == '__main__':
+    ab_path = os.path.abspath("my_files")
+    print(ab_path)
+    read_write_op(ab_path,"file01.txt")
+
+
