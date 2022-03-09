49913055da610026864422c4793254c8960d7b79
Janki <jankipatel2609@gmail.com>
2022-03-08 21:30:01 -0500

    Delete shailja_work directory

---
+++ Exercises_Answer/Exercise24.py
@@ -0,0 +1,27 @@
+"""
+Define a function that can accept two strings as input and print the string with maximum length in console.
+If two strings have the same length, then the function should print all strings line by line.
+
+Hints:
+
+Use len() function to get the length of a string
+
+Solution def printValue(s1,s2): len1 = len(s1) len2 = len(s2) if len1>len2:
+print s1 elif len2>len1: print s2 else: print s1 print s2
+
+printValue("one","three")
+
+"""
+def printVal(s1,s2):
+    len1 = len(s1)
+    len2 = len(s2)
+    if len1 > len2:
+        print(s1)
+    elif len1 < len2:
+        print(s2)
+    else:
+        print(s1)
+        print(s2)
+
+s1,s2=raw_input().split()
+printVal(s1,s2)
